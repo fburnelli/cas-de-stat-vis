cb0961539f8e6857ec94fa19ec9847b3a8192d34
filippo burnelli <burnellifilippo@gmail.com>
2024-02-07 18:56:12 +0100

    wrangling.py

--- wrangling.py
+++ wrangling.py
@@ -1,22 +1,30 @@
 
 def handling_accuracy(data):
-    # Code for handling completeness
+    # Code for handling accuracy
+    # formats and types
+    
     return data
 
 def handling_completeness(data):
     # Code for handling completeness
+    # missing columns
+    # imputations
+    
     return data
 
 def handling_integrity(data):
     # Code for handling integrity
+    # business rules
     return data
 
 def handling_text(data):
     # Code for handling text
+    # harmonisation,business rules, deduplication
     return data
 
 def data_protection(data):
     # Code for data protection
+    # anonymisation,data retention
     return data
 
 
@@ -26,6 +34,7 @@ def wrangling(data):
     data = handling_integrity(data)
     data = handling_text(data)
     data = data_protection(data)
+    
     return data
 
 if __name__ == "__main__":
